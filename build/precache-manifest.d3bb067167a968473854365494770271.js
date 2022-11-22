52f65863644ed002e324932bc89ebafb59726a6d
Jhonatamsoares <jhonatamtecnologias@gmail.com>
2022-11-21 21:21:48 -0300

    excluir sem querer

---
+++ build/precache-manifest.d3bb067167a968473854365494770271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e04bb0ddf961556f508758e2ff6a16f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533a0ef83ab89df80623",
+    "url": "/static/css/main.27ac08eb.chunk.css"
+  },
+  {
+    "revision": "bf8d62607dad0e240a94",
+    "url": "/static/js/2.00e8f1d0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.00e8f1d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533a0ef83ab89df80623",
+    "url": "/static/js/main.d0bf89de.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
